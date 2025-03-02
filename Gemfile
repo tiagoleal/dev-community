754f2e926578f7defa0486607624eb6968746263
Tiago Leal <tiago.sleal12@gmail.com>
2025-03-02 14:19:40 -0300

    feat: custom authentication user and set pagination list of users profile

--- Gemfile
+++ Gemfile
@@ -19,12 +19,12 @@ gem "jbuilder"
 # Use Redis adapter to run Action Cable in production
 # gem "redis", ">= 4.0.1"
 
-gem 'faker'
-gem 'ransack'
-gem 'devise'
-gem 'kaminari'
-gem 'cssbundling-rails'
-gem 'pry'
+gem "faker"
+gem "ransack"
+gem "devise"
+gem "kaminari"
+gem "cssbundling-rails"
+gem "pry"
 
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
@@ -51,11 +51,12 @@ group :development, :test do
   # Omakase Ruby styling [https://github.com/rails/rubocop-rails-omakase/]
   gem "rubocop-rails-omakase", require: false
 
-  #DEBUG
-  gem 'debase'
-  gem 'ruby-debug-ide'
-  gem 'solargraph-rails'
-  gem 'benchmark-ips'
+  # DEBUG
+  gem "debase"
+  gem "ruby-debug-ide"
+  gem "solargraph-rails"
+  gem "benchmark-ips"
+  gem "letter_opener"
 end
 
 group :development do
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,13 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :trackable
+         :recoverable, :rememberable, :validatable, :trackable, :confirmable, authentication_keys: [ :login ]
 
-  has_many :work_experiences
+  has_many :work_experiences, dependent: :destroy
   has_many :connections, dependent: :destroy
+
+  validates :first_name, :last_name, :profile_title, presence: true
+  validates :username, presence: true, uniqueness: true
 
   PROFILE_TITLE = [
     "Senior Ruby on Rails Developer",
@@ -14,11 +17,42 @@ class User < ApplicationRecord
     "Junior Full Stack Ruby on Rails Developer"
   ].freeze
 
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if (login = conditions.delete(:login))
+      where(conditions).where([ "lower(username) = :value OR lower(email) = :value", { value: login.strip.downcase } ]).first
+    elsif conditions.key?(:username) || conditions.key?(:email)
+      where(conditions.to_h).first
+      # conditions[:email]&.downcase!
+      # find_by(conditions)
+    end
+  end
+
+  def connected_user_ids
+    connections.pluck(:connected_user_id)
+  end
+
+  def connected_users
+    User.where(id: connected_user_ids)
+  end
+
+  def self.not_connected_users(user)
+    where.not(id: user.connected_user_ids + [ user.id ])
+  end
+
   def name
     "#{first_name} #{last_name}".strip
   end
 
   def address
+    return nil if city.blank? && state.blank? && country.blank? && pincode.blank?
+
     "#{city}, #{state}, #{country}, #{pincode}"
   end
 
